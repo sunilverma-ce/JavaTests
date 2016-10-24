60a63a42c3197e4acd9bc541d67db7505c8eed44
Sunil K Verma <sunilverma.ce@gmail.com>
2016-10-24 12:47:44 +0530

    updated array package info

--- src/main/java/arrays/package-info.java
+++ src/main/java/arrays/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
+ * This package is having test programs for Arrays and operations that can be performed on arrays.
  */
 /**
  * @author Sunil Verma
- *
+ * @version 0.1
  */
 package arrays;
